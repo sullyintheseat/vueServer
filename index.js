df40e31f957e1dd80e61d82932c1fe401d22747a
zirra <msullivan@heysulli.com>
2019-04-18 18:45:48 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+const port = process.env.PORT || 3139;
+const hostname = '127.0.0.1';
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
